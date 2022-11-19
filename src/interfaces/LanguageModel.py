93cecf0dc545f3841ffa057cb733756c1f49f379
Kogasa <kogasapls@gmail.com>
2022-11-19 13:06:04 -0600

    make language model interface abstract

--- src/interfaces/LanguageModel.py
+++ src/interfaces/LanguageModel.py
@@ -4,12 +4,6 @@ from typing import Any
 
 
 class LanguageModel(metaclass=ABCMeta):
-    """
-    Abstract class for a language model.
-    """
-
-    model: Any
-
     @abstractmethod
     def generate(self, input: Any) -> Any:
         pass
