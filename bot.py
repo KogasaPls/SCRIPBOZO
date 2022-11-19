d79e92cd6ef3956cacdc4658476bcdcab52f0902
Kogasa <kogasapls@gmail.com>
2022-11-19 13:15:21 -0600

    cleanup

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+import argparse
 import logging
 from argparse import Namespace
 
@@ -12,16 +13,25 @@ from twitchio.ext import commands
 log: logging.Logger = CustomLogger(__name__).get_logger()
 
 
+def get_args_from_env() -> argparse.Namespace:
+    arg_parser: ArgParser = ArgParser()
+    arg_parser.add_argument("--nick", envvar="NICK")
+    arg_parser.add_argument("--client_id", envvar="CLIENT_ID")
+    arg_parser.add_argument("--client_secret", envvar="CLIENT_SECRET")
+    arg_parser.add_argument("--initial_channels", envvar="CHANNELS")
+    return arg_parser.parse_args()
+
+
 class Bot(commands.Bot):
-    args: Namespace = ArgParser().get_args_from_env()
+    args: Namespace = get_args_from_env()
     handler: MessageHandler
     auth: TwitchAuth = TwitchAuth(
         client_id=args.client_id,
         client_secret=args.client_secret,
     )
 
-    def __init__(self, handler: MessageHandler) -> None:
-        self.handler = handler
+    def __init__(self, message_handler: MessageHandler) -> None:
+        self.handler = message_handler
         super().__init__(
             client_secret=self.args.client_secret,
             client_id=self.args.client_id,
